c32e0b79e9952a8fa83a5f2005ea4caf65b0c005
WangXF <wangxf0@hotmail.com>
2019-01-07 11:27:46 +0800

    init commit

---
+++ initial/src/main/java/hello/TimePrinter.java
@@ -0,0 +1,31 @@
+package hello;
+
+import java.util.Date;
+
+public class TimePrinter implements Runnable{
+    int pauseTime;
+    String name;
+    public TimePrinter(int x, String n){
+        pauseTime = x;
+        name = n;
+    }
+    public synchronized void run(){
+        while (true){
+            System.out.println(name + ":" + new Date(System.currentTimeMillis()));
+            try {
+                Thread.sleep(pauseTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Thread t1=new Thread(new TimePrinter(1000,"fast guy"));
+        t1.start();
+        Thread t2=new Thread(new TimePrinter(3000,"slow guy"));
+        t2.start();
+
+    }
+}
